5b463552e0defa8583bd5d4a6ec12ffbf0592e66
Kavan Koh <kavanohk@gmail.com>
2020-03-24 18:17:36 +0800

    Update stage redshift script

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,26 +1,60 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
+
 
 class StageToRedshiftOperator(BaseOperator):
+    """
+    Extracts json data from S3 and loads to staging tables in Redshift data warehouse.
+    """
+
     ui_color = '#358140'
+    s3_path = "s3://{}/{}"
+    clear_table_sql = "DELETE FROM {}"
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        REGION AS '{}'
+        FORMAT as json '{}'
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",
+                 s3_bucket="",
+                 s3_key="",
+                 copy_json_option="auto",
+                 region="",
                  *args, **kwargs):
-
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.copy_json_option = copy_json_option
+        self.region = region
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
 
+        redshift.run(self.clear_table_sql.format(self.table))
 
-
-
-
+        rendered_key = self.s3_key.format(**context)
+        s3_path = self.s3_path.format(self.s3_bucket, rendered_key)
+        load_redshift_sql = self.copy_sql.format(
+            self.table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            self.region,
+            self.copy_json_option
+        )
+        redshift.run(load_redshift_sql)
